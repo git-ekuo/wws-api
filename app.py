028fd565f05c4e43d9d4af894234151432e2369f
Ted Kuo <itekuo@gmail.com>
2018-10-29 14:28:49 +0800

    Simplify app.py

--- app.py
+++ app.py
@@ -109,101 +109,6 @@ def get_subset(lat, lon):
     return None, None, average_temp.var167
 
 
-# Get city list
-citylist = pd.read_csv(os.path.join('data', 'simplemaps-worldcities-basic.csv'))
-citylist = citylist.sort_values('pop', ascending=False)
-citylist['loc'] = citylist['city'] + ', ' + citylist['country']
-citylist = citylist.rename(columns={'lat': 'Lat', 'lng': 'Lon'})
-city_options = [{'label': city, 'value': index} for city, index in zip(citylist['loc'], citylist.index)]
-
-app = dash.Dash(__name__, assets_folder='assets', static_folder='assets')  # static url path is 'assets' by default
-app.title = 'Oiko Lab'
-server = app.server
-year = 2017
-
-# Bootstrap CSS
-# Create global chart template & layout
-app.css.append_css({'external_url': 'https://fonts.googleapis.com/css?family=Open+Sans|Roboto'})
-mapbox_access_token = 'pk.eyJ1Ijoiam9lLW1hZ3BpZXNvbHV0aW9ucy1jbyIsImEiOiJjajdqampocnEyM2tuMzNwZHd2MTdia244In0.fVrkGAPX0qqXzET1x6xL0A'
-
-# Create app layout
-app.layout = html.Div(
-    # Controls
-    children=[
-        html.Div(
-            className='four columns container-input',
-            children=[html.Div(
-                children=[
-                    html.Div(id='latlon_dropdown_text',
-                             style={'display': 'block',
-                                    'margin-bottom': '20px'},
-                             children='Where do you stay?'),
-                    dcc.Dropdown(id='latlon_dropdown',
-                                 options=city_options,
-                                 placeholder='Your city',
-                                 value=''
-                                 ),
-                    html.Div(
-                        children=[html.Div(
-                            [html.Div(id='elec_slider_text_question',
-                                      children='How much do you pay for power per month?',
-                                      style={'display': 'block',
-                                             'margin': 'auto',
-                                             'margin-top': '40px',
-                                             'margin-bottom': '20px',
-                                             }),
-                             html.Div(
-                                 style={'height': '40px'},
-                                 children=dcc.RangeSlider(id='elec_slider', min=0, max=300, step=5,
-                                                          allowCross=False,
-                                                          marks={0: '$0',
-                                                                 50: '$50',
-                                                                 100: '$100',
-                                                                 150: '$150',
-                                                                 200: '$200',
-                                                                 250: '$250',
-                                                                 300: '$300'},
-                                                          value=[50, 120])
-                             ),
-                             html.Div(id='elec_slider_text',
-                                      style={'display': 'block',
-                                             'margin-bottom': '20px',
-                                             'text-align': 'center',
-                                             'color': '#999'})]
-                        )],
-                        style={'height': '120px', 'margin-bottom': '40px'}
-                    ),
-                    html.Div(id='elec_advice_text',
-                             style={'display': 'block',
-                                    'margin': 'auto',
-                                    'font-size': '2em',
-                                    'text-align': 'left'}),
-                ]
-            ),
-            ]
-
-        )
-        ,
-        # Charts based on the controls
-        html.Div(
-            id='section-chart',
-            # Fix the height, because dash charts height is weirdly large when there is no data.
-            style={'height': '376px'},
-            className='eight columns',
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(style={'margin-bottom': '40px'},
-                                 children=[dcc.Graph(id='elec_usage', config={'displayModeBar': False})],
-                                 )
-                    ]
-                )
-            ]
-        )
-    ]
-)
-
-
 def calculate_electricity(average_temp, electricity_bill_min, electricity_bill_max):
     """
     Given the daily average temperature data, minimum and maximum an user pay for her power bill, we can work out
@@ -226,18 +131,18 @@ def calculate_electricity(average_temp, electricity_bill_min, electricity_bill_m
 
 
 @app.callback(Output(component_id='elec_usage', component_property='figure'),
-              [Input(component_id='latlon_dropdown', component_property='value'),
-               Input(component_id='elec_slider', component_property='value')])
-def update_elec(location, elec_use):
+              [Input(component_id='latlon_dropdown', component_property='value')])
+def update_elec(location):
+    city_list = _get_city_options()
     if location:
-        lat = citylist.loc[location]['Lat']
-        lon = citylist.loc[location]['Lon']
+        lat = city_list.loc[location]['Lat']
+        lon = city_list.loc[location]['Lon']
     else:
         lat = 42.36
         lon = -71.06
     if location:
-        lat = citylist.loc[location]['Lat']
-        lon = citylist.loc[location]['Lon']
+        lat = city_list.loc[location]['Lat']
+        lon = city_list.loc[location]['Lon']
         color_capacity = 0.7
         color_border_capacity = 1.0
     else:
@@ -246,10 +151,10 @@ def update_elec(location, elec_use):
         color_capacity = 0.05
         color_border_capacity = 0.05
 
-    winterT, summerT, aveT = get_subset(year, lat, lon)
+    winterT, summerT, aveT = get_subset(lat, lon)
 
-    elec_min = elec_use[0]
-    elec_max = elec_use[1]
+    elec_min = 100
+    elec_max = 150
     elec_base, elec_HVAC = calculate_electricity(aveT, elec_min, elec_max)
 
     return {
@@ -286,34 +191,6 @@ def update_elec(location, elec_use):
                       },
         }
     }
-
-
-@app.callback(
-    dash.dependencies.Output('elec_slider_text', 'children'),
-    [dash.dependencies.Input('elec_slider', 'value')])
-def update_output(value):
-    return '$%d to $%d' % (value[0], value[1])
-
-
-@app.callback(
-    dash.dependencies.Output('elec_advice_text', 'children'),
-    [Input(component_id='latlon_dropdown', component_property='value'),
-     Input(component_id='elec_slider', component_property='value')])
-def update_electricity_advice_text(location, elec_use):
-    if location:
-        lat = citylist.loc[location]['Lat']
-        lon = citylist.loc[location]['Lon']
-    else:
-        return ''
-
-    winterT, summerT, aveT = get_subset(year, lat, lon)
-
-    elec_min = elec_use[0]
-    elec_max = elec_use[1]
-    electricity_baseline, electricity_hvac = calculate_electricity(aveT, elec_min, elec_max)
-    total_for_hvac = electricity_hvac.sum()
-
-    return 'That\'s roughly $%s for cooling your home' % int(total_for_hvac)
 
 
 """
@@ -424,6 +301,7 @@ def _get_city(city_name):
             return province
 
     return None
+
 
 def _get_data_set(local_year, local_month, city_name, local_data_path):
     """
